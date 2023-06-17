87d480cde05280c49a8d0228a9e45b45826d86b1
Jendukkie <abduldahanovajenya01@gmail.com>
2023-06-17 15:53:10 +0600

    gg

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -2,13 +2,8 @@ import './Header.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React, { useState } from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap'
-
-import { Link } from 'react-router-dom'
-
-
-
-
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [expanded, setExpanded] = useState(false);
@@ -16,25 +11,32 @@ const Header = () => {
   const handleToggle = () => {
     setExpanded(!expanded);
   };
-    return (
 
-        <>
-   <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" expanded={expanded}>
-      <Container>
-        <Navbar.Brand href="/">Logo</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={handleToggle} />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Link to="/" className="nav-link" onClick={() => setExpanded(false)}>Home</Link>
-            <Link to="/about" className="nav-link" onClick={() => setExpanded(false)}>About</Link>
-            <Link to="/contact" className="nav-link" onClick={() => setExpanded(false)}>Contact</Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+  return (
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" expanded={expanded} fixed="top">
+        <Container>
+          <Navbar.Brand href="/">Logo</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={handleToggle} />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Link to="/" className="nav-link" onClick={() => setExpanded(false)}>
+                Главная
+              </Link>
+              <Link to="/about" className="nav-link" onClick={() => setExpanded(false)}>
+                О нас
+              </Link>
+              <Link to="/contact" className="nav-link" onClick={() => setExpanded(false)}>
+                Войти/Зарегистрироваться 
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
-        </>
-    )
-}
 export default Header;
+
 
