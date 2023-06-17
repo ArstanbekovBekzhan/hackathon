2b2f159a3ffd94cc96b4437e86fced07c845613a
Jendukkie <abduldahanovajenya01@gmail.com>
2023-06-17 12:00:13 +0600

    head-foot

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,20 +1,28 @@
-import React, { useEffect, useRef } from 'react';
-import HStyle from "./Header.module.css";
+import React from 'react';
+import "./Header.css";
 
 const Header = ({ onHeaderHeight }) => {
-  const headerRef = useRef(null);
-
-  useEffect(() => {
-    if (headerRef.current) {
-      const height = headerRef.current.clientHeight;
-      onHeaderHeight(height);
-    }
-  }, [onHeaderHeight]);
-
   return (
-    <header className={HStyle.header} ref={headerRef}>
-      header
-    </header>
+    <div>
+    <section class="top-nav">
+      <div>Airngas Controls</div>
+      <input id="menu-toggle" type="checkbox" />
+      <label class="menu-button-container" for="menu-toggle">
+        <div class="menu-button"></div>
+      </label>
+      <ul class="menu">
+        <li>
+          <a href="#products">Products</a>
+        </li>
+        <li>
+          <a href="#whyus">Why Us?</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </section>
+  </div>
   );
 };
 
