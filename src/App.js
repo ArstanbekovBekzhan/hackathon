5c2806bb333ee0ceb9c5653144f5aa191f6e1b9d
iuehwogb <bekzan659@gmail.com>
2023-06-17 14:50:40 +0600

    mm

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import "./App.css"
 import { Homepage } from './pages/Home/Homepage.jsx';
-// import { CardDetails } from './pages/Details/Details';
+import { CardDetails } from './pages/Details/Details';
 import { Layout } from './component/Layout/Layout.jsx'
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Homepage />} />
-          {/* <Route path="/cards/:id" element={<CardDetails />} /> */}
+          <Route path="/cards/:id" element={<CardDetails />} />
         </Route>
       </Routes>
     </>
