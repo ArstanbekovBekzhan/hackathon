5643b452de6100bb61802f7c7b6cc9abc18c1f42
Aiperiwa <periwa003@gmail.com>
2023-06-17 15:58:20 +0600

    add input

--- src/component/Form/index.jsx
+++ src/component/Form/index.jsx
@@ -1,19 +1,38 @@
 import React from "react";
 import {Link} from "react-router-dom"
 import './form.css'
+import {useState} from React
 
 const Form = () => {
+
+  const [status, setStatus] = useState(false)
+
+
   return (
     <div className="content">
       <form action="" className="form">
         <h2 className="form_title">Регистрация</h2>
-        <h2 className="form_title">Придумай проль для входа на любом устройстве</h2>
-        <input className="form_field" type="text" />
-        <button className="form_btn" type="button">Продолжить</button>
-        <input className="form_field" type="text" />
-        <button className="form_btn" type="submit">Создать аккаунт</button>
+        <h2 className="form_title">
+          {
+            status ? 'Придумай пароль для входа на любом устройстве' : 'Регистрация'
+          }
+        </h2>
 
-        <Link to="/login">У меня есть аккаунт</Link>
+        {
+          status ? <>
+              <input className="form_field" placeholder="Придумайте пароль" type="text" />
+              <button className="form_btn" type="submit">Создать аккаунт</button>
+
+          </> : <>
+              <input className="form_field" placeholder="Введите Email" type="text" />
+              <button onClick={() => setStatus(true)} className="form_btn" type="button">Продолжить</button>
+              <Link to="/login">У меня есть аккаунт</Link>
+          </>
+        }
+       
+        
+
+        
 
 
 
