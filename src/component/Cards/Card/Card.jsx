3934cab1ef150c4cc86f3b91e021669b4ec248a5
AzizChiderov <chiderovaziz@gmail.com>
2023-06-17 21:21:46 +0600

    add private

--- src/component/Cards/Card/Card.jsx
+++ src/component/Cards/Card/Card.jsx
@@ -16,7 +16,7 @@ const Card = ({ cards }) => (
           <h4>{card.Nick}</h4>
           </div>
           <div className={SCard.made}>
-            <img src={card.made ? 'https://cdn-icons-png.flaticon.com/512/1280/1280891.png' : 'https://w7.pngwing.com/pngs/206/841/png-transparent-multiplication-sign-symbol-x-mark-symbol-miscellaneous-angle-playstation-4.png'} alt="" />
+            <img src={card.made ? 'https://cdn-icons-png.flaticon.com/512/1280/1280891.png' : 'https://w7.pngwing.com/pngs/206/841/png-transparent-multiplication-sign-symbol-x-mark-symbol-miscellaneous-angle-playstation-4.png'} alt="#" />
           </div>
         </div>
       </NavLink>
--- src/component/PrivateCabinet/Private.jsx
+++ src/component/PrivateCabinet/Private.jsx
@@ -1,27 +1,49 @@
-import React from 'react'
-import p from "./Private.module.css"
-
+import React, { useState, useEffect } from 'react';
+import p from "./Private.module.css";
+import axios from 'axios';
 
 const Private = () => {
+  const [done, setDone] = useState(null);
+  const [cards, setCards] = useState([]);
+
+  const handleToggle = (value) => {
+    setDone(value);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/cards');
+        const filteredCards = response.data.filter(card => card.made === done);
+        setCards(filteredCards);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [done]);
+
   return (
     <div className={p.container}> 
-        <div className={p.user_box}>
+      <div className={p.user_box}>
         <img src="https://thumbs.dfs.ivi.ru/storage23/contents/d/f/3b0d9897433be7b674d72b78bc0087.jpg" alt="#" />
         <h2 className={p.name}>Johnatan</h2>
-        <div className={p.red_box}>
-          <div className={p.red_circle}></div>
-          <div className={p.red_circle}></div>
-        </div>
-        <div className={p.green_box}>
-          <div className={p.green_circle}></div>
-          <div className={p.green_circle}></div>
-        </div>
-        </div>
-        <div className={p.card_desk}>
-            <button className={p.add_btn}>Добавить событие</button>
-        </div>
+        <button className={p.green_box} onClick={() => handleToggle(true)}>Выполнено</button>
+        <button className={p.red_box} onClick={() => handleToggle(false)}>Не выполнено</button>
+      </div>
+      <div className={p.card_desk}>
+        {cards.map(card => (
+          <div key={card.id} className={p.card}>
+            <img src={card.image} alt="#" />
+            <h3>{card.title}</h3>
+            <div></div>
+          </div>
+        ))}
+      </div>
+      <button className={p.add_btn}>Добавить событие</button>
     </div>
-  )
+  );
 }
 
-export  { Private }
+export { Private };
