d6deb3bdaf395574094593f8ed25cda2bef73981
Aiperiwa <periwa003@gmail.com>
2023-06-17 15:18:14 +0600

    add files

--- src/App.js
+++ src/App.js
@@ -2,29 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import "./App.css"
 import { Homepage } from './pages/Home/Homepage.jsx';
 import { CardDetails } from './pages/Details/Details';
-
-
-import { Layout } from './component/Layout/Layout.jsx'
-
-import { Register } from './pages/Register'
-
-function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Homepage />} />
-          <Route path="/register" element={<Register />}/>
-        </Route>
-      </Routes>
-    </>
-  );
-}
-
-import { Routes, Route } from 'react-router-dom';
-import "./App.css"
-import { Homepage } from './pages/Home/Homepage.jsx';
-import { CardDetails } from './pages/Details/Details';
 import { Layout } from './component/Layout/Layout.jsx'
 
 import { Register } from './pages/Register'
