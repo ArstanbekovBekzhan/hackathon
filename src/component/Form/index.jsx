61fa2efc29f8106b4a37ccd8877256f10dafcc62
Aiperiwa <periwa003@gmail.com>
2023-06-17 17:26:39 +0600

    add register

--- src/component/Form/index.jsx
+++ src/component/Form/index.jsx
@@ -1,41 +1,70 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom"
 import './form.css'
-import {useState} from React
+import { FaPencilAlt } from 'react-icons/fa'
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai'
+import axios from "axios";
 
 const Form = () => {
 
   const [status, setStatus] = useState(false)
+  const [email, setEmail] = useState('')
+  const [eye, setEye] = useState(false)
+  const registerUser = (e) => {
+    e.preventDefault()
+    let newUser = {
+      email,
+      password: e.target[0].value  
 
+    }
+    axios.post('http://localhost:8080/register', newUser)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err.message))
+    console.log()
+  }
 
   return (
     <div className="content">
-      <form action="" className="form">
-        <h2 className="form_title">Регистрация</h2>
+      <form action="" className="form" onSubmit={registerUser}>
+        {
+          status && <p className="form_email" onClick={() => setStatus(false)}>{email}<FaPencilAlt/></p> 
+
+        }
+
         <h2 className="form_title">
           {
             status ? 'Придумай пароль для входа на любом устройстве' : 'Регистрация'
           }
-        </h2>
+
+       </h2>
 
         {
-          status ? <>
-              <input className="form_field" placeholder="Придумайте пароль" type="text" />
+          status && <>
+
+              <div className="form_password">
+                <input className="form_field" placeholder="Придумайте пароль" type={eye ? 'text' : 'password'}/>
+                <span className="form_eye" onClick={() => setEye(prev => !prev)}>
+                  {
+                    eye ? <AiFillEyeInvisible/> : <AiFillEye/>
+
+                  }
+                </span>
+              </div>
               <button className="form_btn" type="submit">Создать аккаунт</button>
 
-          </> : <>
-              <input className="form_field" placeholder="Введите Email" type="text" />
-              <button onClick={() => setStatus(true)} className="form_btn" type="button">Продолжить</button>
+          </>
+        } 
+
+        {
+          !status &&
+           <>
+              <input value={email} onChange={(e) => setEmail(e.target.value)} className="form_field" placeholder="Введите Email" type="text" />
+              <div onClick={() => setStatus(true)} className="form_btn">Продолжить</div>
               <Link to="/login">У меня есть аккаунт</Link>
           </>
         }
        
-        
-
-        
-
-
-
+  
       </form>
 
     </div>
