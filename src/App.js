a4d7b78676e717224791a9c484c43c310a17c3f8
Samy4-byte <samatbayizov@gmail.com>
2023-06-18 09:04:17 +0600

    add modal

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import "./App.css"
 import { Homepage } from './pages/Home/Homepage.jsx';
 import { CardDetails } from './pages/Details/Details';
-import { Layout } from './component/Layout/Layout.jsx';
 import Private from './component/PrivateCabinet/Private.jsx'
 import { PublicServicesDynamic } from './pages/PublicServicesDynamic';
 import { Layout } from './component/Layout/Layout.jsx'
